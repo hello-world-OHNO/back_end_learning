5672e7ae1bc05d3205f8ca09ea3674a24d483976
hello-world-OHNO <daruma0248@gmail.com>
2025-03-03 19:41:04 +0900

    init commit

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE my_database;
+USE my_database;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    age INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
